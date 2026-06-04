3950a8bf32a3ec66550bab6f69c9326916d19ee6
fc54484 <Cwy1104721549@gmail.com>
2026-06-04 02:14:47 +0100

    fix action

--- src/test/java/com/example/meetings/endToEnd/SeleniumTest.java
+++ src/test/java/com/example/meetings/endToEnd/SeleniumTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.transaction.annotation.Transactional;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -21,11 +22,12 @@ import static org.junit.jupiter.api.Assertions.*;
 // DELETE FROM USERS;
 // DELETE FROM MEETING_PARTICIPANTS;
 // DELETE FROM MEETINGS;
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
-@Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) 
 class SeleniumTest {
     private WebDriver driver;
+
+    @LocalServerPort
+    private int port;
 
     @BeforeEach
     void setup() {
@@ -48,15 +50,16 @@ class SeleniumTest {
 
     @Test
     void createMeeting() {
+        String baseUrl = "http://localhost:" + port;
         String username = "user" + System.currentTimeMillis();
 
-        driver.get("http://localhost:8080/register");
+        driver.get(baseUrl + "/register");
         driver.findElement(By.id("username")).sendKeys(username);
         driver.findElement(By.id("email")).sendKeys(username + "@gmail.com");
         driver.findElement(By.id("password")).sendKeys("123");
         driver.findElement(By.tagName("button")).click();
 
-        driver.get("http://localhost:8080/login");
+        driver.get(baseUrl + "/login");
         driver.findElement(By.name("username")).sendKeys(username);
         driver.findElement(By.name("password")).sendKeys("123");
         driver.findElement(By.tagName("button")).click();
@@ -66,7 +69,7 @@ class SeleniumTest {
 
         assertTrue(driver.getCurrentUrl().contains("/calendar"));
 
-        driver.get("http://localhost:8080/meetings/new");
+        driver.get(baseUrl + "/meetings/new");
         driver.findElement(By.id("title")).sendKeys("Meeting");
         driver.findElement(By.id("description")).sendKeys("Description");
         JavascriptExecutor js = (JavascriptExecutor) driver;
